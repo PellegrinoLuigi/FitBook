0c27248f6b7622236bdda98024a591a509e5b25f
Luigi Pellegrino <60132440+PellegrinoLuigi@users.noreply.github.com>
2025-01-15 15:56:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+port = int(os.environ.get("PORT", 5000))  # Usa 5000 come fallback
+app.run(host="0.0.0.0", port=port)
 # Dati predefiniti (senza input o file)
 name = "Luigi"
 date = "2025-01-20"
