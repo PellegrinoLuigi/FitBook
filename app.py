daf99fbb79d71740716f245db7fb93969a50df2e
lpellegrino <pelle.l33@gmail.com>
2025-01-24 13:33:58 +0100

    @AggiuntaConnessioneDB

--- app.py
+++ app.py
@@ -3,6 +3,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
 
+# Configurazione del database
+DB_CONFIG = {
+    'dbname': 'fitbook-reservation',
+    'user': 'fitbook_reservation_user',
+    'password': 'W858EmxOR0Hp34YiTAW5HLpaQNoK5qW9',
+    'host': 'dpg-cu3s0ptds78s73ehhd8g-a',
+    'port': 5432  # Default PostgreSQL port
+}
+
+def get_users():
+    """Recupera i dati dalla tabella user."""
+    conn = psycopg2.connect(**DB_CONFIG)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, nome, cognome, email, data_di_nascita FROM user;")
+    rows = cursor.fetchall()  # Recupera tutti i record
+    conn.close()
+    console.log(rows)
+    return rows
+
 @app.route('/')
 def home():
     return render_template('index.html')
