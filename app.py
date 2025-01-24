9fad448860746b904a022d12ce7e7880600367dd
lpellegrino <pelle.l33@gmail.com>
2025-01-24 15:51:39 +0100

    registraz

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def registra_utente(nome, cognome, email, data_nascita, password):
         print(f"Errore nel registrare l'utente: {e}")
         return False
 
+
 @app.route('/register', methods=['POST'])
 def registrazione():
     data = request.get_json()
@@ -61,7 +62,8 @@ def registrazione():
     if verifica_email(email):
         return jsonify({"success": False, "message": "L'email è già registrata."})
     
-    if registra_utente(nome, cognome, email, data_nascita, password):
+     # if registra_utente(nome, cognome, email, data_nascita, password):
+    if registra_utente('Mario', 'Rossi', 'mario.rossi@example.com', '1990-05-20', 'hashed_password'):
         return jsonify({"success": True, "message": "Registrazione avvenuta con successo!"})
     else:
         return jsonify({"success": False, "message": "Errore durante la registrazione."})
