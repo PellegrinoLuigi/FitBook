a26e7934ffd2484bca36b5d562bbf1dc49ddac91
lpellegrino <pelle.l33@gmail.com>
2025-01-24 14:28:12 +0100

    mod

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def get_users():
 def home():
     users = get_users()
     utente='cavolo'
-    return render_template('index.html')
+    return render_template('index.html',utente=utente, users=users)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Usa la porta specificata da Render o 5000 in locale
