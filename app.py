eb6a453444c2054a6eb89cd4642be3940ad0d6ef
lpellegrino <pelle.l33@gmail.com>
2025-01-30 18:30:19 +0100

    reservation_date

--- app.py
+++ app.py
@@ -151,10 +151,11 @@ def check_reservation():
         user_email = data.get('userName')
         reservation_date = data.get('reservation_date')
         reservation_date2 = data.get('reservation_date')
-        resdate='2025-01-31'     
 
       
-        result =db_request_select_all2(QUERY_CHECK_RESERVATION,resdate,reservation_date2,user_email)
+       # result =db_request_select_all2(QUERY_CHECK_RESERVATION,resdate,reservation_date2,user_email)
+        result =db_request_select_all(QUERY_CHECK_RESERVATION,(reservation_date,reservation_date2,user_email))
+
        # result =db_request_select_all2(query_test,resdate,resdate)
 
         if result:
